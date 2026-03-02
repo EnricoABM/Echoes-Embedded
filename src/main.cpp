604adee48030c402ab1b92d9d427018b1f58a857
Avraham <souzaavraham@proton.me>
2026-03-02 13:37:57 -0300

    feat: little setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
-void app_main() {}
+#include <Arduino.h>
+
+extern "C" void app_main(void) {
+  initArduino();
+
+  pinMode(2, OUTPUT);
+  Serial.begin(115200);
+
+  while (true) {
+    digitalWrite(2, HIGH);
+    vTaskDelay(pdMS_TO_TICKS(500));
+    digitalWrite(2, LOW);
+    vTaskDelay(pdMS_TO_TICKS(500));
+    Serial.println("Hello World!");
+  }
+}
